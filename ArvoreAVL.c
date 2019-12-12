872bcbd27e34ed7ddaed7a40726fef006ea50dc9
Tadei <gustavo.tadei@unir.br>
2019-12-12 11:14:27 -0400

    Header e Implementacao da Arvore AVL concluidos sem Erro de Compilacao. Falta terminar o main

--- ArvoreAVL.c
+++ ArvoreAVL.c
@@ -51,6 +51,42 @@ int fatorBalanceamentoNo(struct NO* no){
     return alturaNo(no->esq) - alturaNo(no->dir);
 }
 
+void rotacaoSimplesEsquerda(Arvore *A){
+    struct NO *B;
+    B = (*A)->dir;
+
+    (*A)->dir = B->esq;
+    B->esq = (*A);
+
+    (*A)->altura = maior(alturaNo( (*A)->esq ), alturaNo( (*A)->dir) ) + 1;
+    B->altura = maior(alturaNo( B->dir ), (*A)->altura) +1;
+
+    (*A) = B;
+}
+
+void rotacaoSimplesDireita(Arvore *A){
+    struct NO *B;
+    B = (*A)->esq;
+
+    (*A)->esq = B->dir;
+    B->dir = *A;
+
+    (*A)->altura = maior(alturaNo( (*A)->esq ), alturaNo( (*A)->dir )) + 1;
+    B->altura = maior( alturaNo(B->esq), (*A)->altura) + 1;
+
+    (*A) = B;
+}
+
+void rotacaoDuplaEsquerdaDireita(Arvore *A){
+    rotacaoSimplesEsquerda(&(*A)->esq);
+    rotacaoSimplesDireita(A);
+}
+
+void rotacaoDuplaDireitaEsquerda(Arvore *A){
+    rotacaoSimplesDireita( &(*A)->dir );
+    rotacaoSimplesEsquerda(A);
+}
+
 //////////////////////////////////////////////////
 /**< Funções do Header da Árvore */
 //////////////////////////////////////////////////
@@ -63,8 +99,6 @@ void destruir(Arvore *raiz){
     destruirNo(*raiz);
     free(raiz);
 }
-
-int insere(Arvore *raiz, int valor);
 
 int vazia(Arvore *raiz){
     if (raiz == NULL) return 1;
@@ -107,7 +141,7 @@ int buscaNo(Arvore *raiz, int valor){
     while (atual != NULL){
         if (atual->info == valor){
             printf("Encontrou!\n");
-            printf("No -> Valor: %d\n", (*raiz)->info));
+            printf("No -> Valor: %d\n", (*raiz)->info);
             return 1;
         }
 
@@ -120,7 +154,7 @@ int buscaNo(Arvore *raiz, int valor){
         }
     }
 
-    printf("Nao Encontrou.\n");,
+    printf("Nao Encontrou.\n");
     return 0;
 }
 
@@ -128,7 +162,7 @@ void exibePreOrdem(Arvore *raiz){
     if (raiz == NULL) return;
 
     if (*raiz != NULL){
-        printf("No -> Valor: %d\n", (*raiz)->info));
+        printf("No -> Valor: %d\n", (*raiz)->info);
 
         exibePreOrdem( &((*raiz)->esq) );
         exibePreOrdem( &((*raiz)->dir) );
@@ -141,7 +175,7 @@ void exibeInOrdem(Arvore *raiz){
     if (*raiz != NULL){
         exibeInOrdem( &((*raiz)->esq) );
 
-        printf("No -> Valor: %d\n", (*raiz)->info));
+        printf("No -> Valor: %d\n", (*raiz)->info);
 
         exibeInOrdem( &((*raiz)->dir) );
     }
@@ -154,6 +188,61 @@ void exibePosOrdem(Arvore *raiz){
         exibePosOrdem( &((*raiz)->esq) );
         exibePosOrdem( &((*raiz)->esq) );
 
-        printf("No -> Valor: %d\n", (*raiz)->info));
+        printf("No -> Valor: %d\n", (*raiz)->info);
     }
+}
+
+int insere(Arvore *raiz, int valor){
+    int res;
+
+    //Arvore Vazia ou No Folha
+    if (*raiz == NULL){
+        struct NO *novo;
+        novo = (struct NO*)malloc( sizeof(struct NO) );
+
+        if (novo == NULL) return 0; //Ocorreu erro
+
+        novo->info = valor;
+        novo->altura = 0;
+        novo->esq = NULL;
+        novo->dir = NULL;
+
+        *raiz = novo;
+
+        return 1; //Inseriu com Sucesso
+    }
+
+    struct NO *atual = *raiz;
+
+    if (valor < atual->info){
+        if( (res = insere(&(atual->esq), valor) ) == 1){
+            if (fatorBalanceamentoNo(atual) >= 2){
+                if (valor < (*raiz)->esq->info){
+                    rotacaoSimplesDireita(raiz);
+                }
+                else {
+                    rotacaoDuplaEsquerdaDireita(raiz);
+                }
+            }
+        }
+    } else {
+        if (valor > atual->info){
+            if ( (res = insere(&(atual->dir), valor)) == 1 ){
+                if (fatorBalanceamentoNo(atual) >= 2){
+                    if (valor > (*raiz)->dir->info){
+                        rotacaoSimplesEsquerda(raiz);
+                    }else {
+                        rotacaoDuplaDireitaEsquerda(raiz);
+                    }
+                }
+            }
+        } else {
+            printf ("O valor informado ja existe na Arvore.\n");
+            return 0;
+        }
+    }
+
+    atual->altura = maior( alturaNo(atual->esq), alturaNo(atual->dir) ) + 1;
+
+    return res;
 }
